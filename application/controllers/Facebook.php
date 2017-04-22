b907398d2d2ffaf8b9058318e5736f9c564b65f8
Kurniawan Yudha Putrama <kurniawan.yudha.p@gmail.com>
2017-04-22 12:51:06 +0700

    rename example to facebook

--- application/controllers/Facebook.php
+++ application/controllers/Facebook.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * @property  Facebook facebook
  */
-class Example extends CI_Controller {
+class Facebook extends CI_Controller {
 
 	public function __construct()
 	{
